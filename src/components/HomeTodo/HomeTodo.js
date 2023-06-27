e7ff17f780ad851539e79624afa3789e01bc368f
“slaptashka” <slaptashkaa@gmail.com>
2023-06-27 14:53:59 +0300

    added messages if there are no tasks to display

--- src/components/HomeTodo/HomeTodo.js
+++ src/components/HomeTodo/HomeTodo.js
@@ -1,29 +1,54 @@
-import React from "react";
 import styles from "./HomeTodo.module.css";
 
 import HomeTodoItem from "../HomeTodoItem/HomeTodoItem";
 
 export default function HomeTodo({ todos, setTodos, completed }) {
-  return (
-    <div className={styles.block}>
-      {todos.map((todo) => {
-        if (completed === 1 && !todo.isDone) {
-          return "";
-        } else if (completed === 2 && todo.isDone) {
-          return "";
-        } else if (completed === 3 && !todo.inFavorite) {
-          return "";
-        }
+  let hasItemsToShow = false;
 
-        return (
-          <HomeTodoItem
-            key={todo.id}
-            todo={todo}
-            todos={todos}
-            setTodos={setTodos}
-          />
-        );
-      })}
+  const renderedItems = todos.map((todo) => {
+    if (completed === 1 && !todo.isDone) {
+      return null;
+    } else if (completed === 2 && todo.isDone) {
+      return null;
+    } else if (completed === 3 && !todo.inFavorite) {
+      return null;
+    }
+
+    hasItemsToShow = true;
+
+    return (
+      <HomeTodoItem
+        key={todo.id}
+        todo={todo}
+        todos={todos}
+        setTodos={setTodos}
+      />
+    );
+  });
+
+  const showTodos = hasItemsToShow ? (
+    renderedItems
+  ) : (
+    <div className={styles.blockNoDisplay}>
+      {todos.length === 0 ? (
+        <p className={styles.textNoDisplay}>
+          You don't have tasks, but you can create them. 😊
+        </p>
+      ) : completed === 1 ? (
+        <p className={styles.textNoDisplay}>
+          You don't have completed tasks. 😔
+        </p>
+      ) : completed === 2 ? (
+        <p className={styles.textNoDisplay}>
+          You don't have not completed tasks. 🥰
+        </p>
+      ) : completed === 3 ? (
+        <p className={styles.textNoDisplay}>
+          You don't have any tasks in your favorites. 🙄
+        </p>
+      ) : null}
     </div>
   );
+
+  return <div className={styles.block}>{showTodos}</div>;
 }
